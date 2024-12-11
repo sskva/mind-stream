3d6f3a93d7a165874ec6089c82a1106f6d5ed0b8
ssk <ssk29@yandex.ru>
2024-12-11 06:56:56 +0300

    Initial commit

---
+++ src/main/java/ru/ssk/mind_stream/MindStreamApplication.java
@@ -0,0 +1,13 @@
+package ru.ssk.mind_stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ssk/mind_stream/MindStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ssk.mind_stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
